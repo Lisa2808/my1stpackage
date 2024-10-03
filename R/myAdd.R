16c7497d2a788d080dd97d7ca1c22bb76432d3de
lisa-hoelt <lholtinger@icloud.com>
2024-10-03 14:47:41 +0200

    minor changes

--- R/myAdd.R
+++ R/myAdd.R
@@ -1,3 +1,13 @@
+#' My Add function
+#'
+#' @param a first summand
+#' @param b second summan
+#'
+#' @return the sum will be returned
+#' @export
+#'
+#' @examples
+#' myAdd(2,3)
 myAdd <- function(a,b){
   return(a+b)
 }
---
+++ tests/testthat/test-myAdd.R
@@ -0,0 +1,3 @@
+test_that("addition works", {
+  expect_equal(myAdd(2,3), 5)
+})
